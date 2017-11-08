97e90805106fc4a1de6245262c886aa9c045cad6
Lauryn McFarland <lauryn@Lauryns-MacBook-Pro.local>
2017-11-08 13:50:05 -0600

    added array to count guesses, logic to check for duplicate guesses, and alert to warn of duplicate guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,6 +25,12 @@ var guess;
 // Declares/initializes the start point of the guess/word search
 var startIndex = 0;
 
+//Declare array to store old guesses
+var oldGuess = [];
+
+//Decalre variable to index guesses, start at -1, so first counter will be zero
+var guessCount = -1;
+
 // Objects
 
 // Try to contain the entire game in an object?!
@@ -44,16 +50,24 @@ document.onkeyup = function(event) {
 	document.getElementById("currentGuess").textContent = event.key;
 	// Stores the input "guess" as a string in the variable guess.
 	guess = event.key;
-	console.log("The guess is " + guess);
-	console.log("The word is " + word);
-	console.log("The search will begin at " + startIndex);
+	//check if this character has been guessed before.
+	if (oldGuess.indexOf(guess) !== -1){
+		alert("You've already guessed that...")
+		return //stop running onkeyup function is the character is found in oldGuess
+	}
+	//index this guess
+	guessCount++;
+
+	// console.log("The guess is " + guess);
+	// console.log("The word is " + word);
+	// console.log("The search will begin at " + startIndex);
 
 	
 
 	while (word.indexOf(guess, startIndex) !== -1) { //While guess is found in word after startIndex
-	  	console.log("The word searched was " + word);
+	  	// console.log("The word searched was " + word);
 	  	// replace letter
-	  	console.log("The letter was found at " + word.indexOf(guess,startIndex)); //Where the letter is found
+	  	// console.log("The letter was found at " + word.indexOf(guess,startIndex)); //Where the letter is found
 	  	// console.log(arrayOfUnderscores); //What is in the arrayOfUnderscores before
 	  	arrayOfUnderscores[word.indexOf(guess,startIndex)] = guess;
 	  	// console.log(arrayOfUnderscores); //What is in the arrayOfUnderscores after letter is replaced
@@ -68,11 +82,14 @@ document.onkeyup = function(event) {
 	}
 	
 	//create text node for old guesses
-	var oldGuess = document.createTextNode(guess);
+	var currentGuess = document.createTextNode(guess);
 	//update the text content with current guess and a comma with a space
-    oldGuess.textContent = guess + ", " ;
+    currentGuess.textContent = guess + ", " ;
     //append the text node to the span oldGuess
-    document.getElementById("oldGuess").appendChild(oldGuess);
+    document.getElementById("oldGuess").appendChild(currentGuess);
+    //Store current guess in array with past guesses.
+    oldGuess[guessCount] = guess;
+    console.log(oldGuess);
 
 	var startIndex = 0; // After searching word, set startIndex back to 0.
 };
